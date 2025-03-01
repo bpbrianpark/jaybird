c9df4f9ba1f36f66f11eaf1d926363d5e047aae7
Brian Park <bmp1208@students.cs.ubc.ca>
2025-02-28 16:24:28 -0800

    navbar complete

--- frontend/src/app/components/NavBar.js
+++ frontend/src/app/components/NavBar.js
@@ -1,18 +1,19 @@
 import Link from "next/link";
 import Image from "next/image";
+import NavItem from "./NavItem";
 
 const NavBar = () => {
   return (
     <nav className="bg-white text-orange p-4 flex justify-between items-center">
         <div className="flex items-center">
-            <Image src="/jasonparklogo.svg" alt="Jason Park Photography" width={150} height={50} className="w-full h-25"/>
+            <Image src="/jasonparklogo.svg" alt="Jason Park Photography" width={150} height={50} className="w-full h-22 object-contain min-w-[150px]"/>
         </div>
       <div className="hidden md:flex items-center space-x-25">
-        <Link href="/" className="text-lg hover:text-orange-500">Home</Link>
-        <Link href="/portfolio" className="text-lg hover:text-orange-500">Portfolio</Link>
-        <Link href="/dex" className="text-lg hover:text-orange-500">Dex</Link>
-        <Link href="/contact" className="text-lg hover:text-orange-500">Contact</Link>
-        <Link href="https://instagram.com" target="_blank">
+        <NavItem href="/" text="About" icon="/person.svg"/>
+        <NavItem href="/portfolio" text="Portfolio" icon="/camera.svg"/>
+        <NavItem href="/dex" text="WildDex" icon="/map.svg"/>
+        <NavItem href="/contact" text="Contact" icon="/phone.svg"/>
+        <Link href="https://www.instagram.com/jasonpark778/" target="_blank">
           <Image src="/instagramlogo.svg" alt="Instagram" width={30} height={30} className="w-full h-full mr-2"/>
         </Link>
       </div>
