dcc73c9051d4c53df349c9f847dfe56bd30a7b46
Brian Park <bmp1208@students.cs.ubc.ca>
2025-02-28 16:28:05 -0800

    adjusted instagram

--- frontend/src/app/components/NavBar.js
+++ frontend/src/app/components/NavBar.js
@@ -8,14 +8,17 @@ const NavBar = () => {
         <div className="flex items-center">
             <Image src="/jasonparklogo.svg" alt="Jason Park Photography" width={150} height={50} className="w-full h-22 object-contain min-w-[150px]"/>
         </div>
-      <div className="hidden md:flex items-center space-x-25">
+      <div className="hidden md:flex items-center space-x-15">
         <NavItem href="/" text="About" icon="/person.svg"/>
         <NavItem href="/portfolio" text="Portfolio" icon="/camera.svg"/>
         <NavItem href="/dex" text="WildDex" icon="/map.svg"/>
         <NavItem href="/contact" text="Contact" icon="/phone.svg"/>
-        <Link href="https://www.instagram.com/jasonpark778/" target="_blank">
-          <Image src="/instagramlogo.svg" alt="Instagram" width={30} height={30} className="w-full h-full mr-2"/>
-        </Link>
+        <div className="w-24 h-16 flex flex-col items-center justify-center rounded-lg hover:bg-gray-200 transition">  
+          <Link href="https://www.instagram.com/jasonpark778/" className="flex flex-col items-center space-y-1">
+          <Image src="/instagramlogo.svg" alt="Instagram" width={20} height={20} className="w-1/2 h-1/2 mr-2"/>
+          <span className="text-sm">{"Instagram"}</span>
+          </Link>
+        </div> 
       </div>
 
       <button className="md:hidden text-3xl">☰</button>
